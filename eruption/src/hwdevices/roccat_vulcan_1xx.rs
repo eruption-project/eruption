1c1916a7c1250d83d2e52972b2143373eda9658d
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2021-02-11 13:03:13 +0100

    Fix clippy warning

--- eruption/src/hwdevices/roccat_vulcan_1xx.rs
+++ eruption/src/hwdevices/roccat_vulcan_1xx.rs
@@ -122,37 +122,37 @@ impl RoccatVulcan1xx {
         }
     }
 
-    pub(self) fn query_ctrl_report(&mut self, id: u8) -> Result<()> {
-        trace!("Querying control device feature report");
+    // pub(self) fn query_ctrl_report(&mut self, id: u8) -> Result<()> {
+    //     trace!("Querying control device feature report");
 
-        if !self.is_bound {
-            Err(HwDeviceError::DeviceNotBound {}.into())
-        } else if !self.is_opened {
-            Err(HwDeviceError::DeviceNotOpened {}.into())
-        } else {
-            match id {
-                0x0f => {
-                    let mut buf: [u8; 256] = [0; 256];
-                    buf[0] = id;
+    //     if !self.is_bound {
+    //         Err(HwDeviceError::DeviceNotBound {}.into())
+    //     } else if !self.is_opened {
+    //         Err(HwDeviceError::DeviceNotOpened {}.into())
+    //     } else {
+    //         match id {
+    //             0x0f => {
+    //                 let mut buf: [u8; 256] = [0; 256];
+    //                 buf[0] = id;
 
-                    let ctrl_dev = self.ctrl_hiddev.as_ref().lock();
-                    let ctrl_dev = ctrl_dev.as_ref().unwrap();
+    //                 let ctrl_dev = self.ctrl_hiddev.as_ref().lock();
+    //                 let ctrl_dev = ctrl_dev.as_ref().unwrap();
 
-                    match ctrl_dev.get_feature_report(&mut buf) {
-                        Ok(_result) => {
-                            hexdump::hexdump_iter(&buf).for_each(|s| trace!("  {}", s));
+    //                 match ctrl_dev.get_feature_report(&mut buf) {
+    //                     Ok(_result) => {
+    //                         hexdump::hexdump_iter(&buf).for_each(|s| trace!("  {}", s));
 
-                            Ok(())
-                        }
+    //                         Ok(())
+    //                     }
 
-                        Err(_) => Err(HwDeviceError::InvalidResult {}.into()),
-                    }
-                }
+    //                     Err(_) => Err(HwDeviceError::InvalidResult {}.into()),
+    //                 }
+    //             }
 
-                _ => Err(HwDeviceError::InvalidStatusCode {}.into()),
-            }
-        }
-    }
+    //             _ => Err(HwDeviceError::InvalidStatusCode {}.into()),
+    //         }
+    //     }
+    // }
 
     fn send_ctrl_report(&mut self, id: u8) -> Result<()> {
         trace!("Sending control device feature report");
