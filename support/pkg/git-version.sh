4f640619ddf642537682b3621013fd310b089333
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2021-03-20 15:28:43 +0100

    Update format of git package
    Update the format of the package version to more closely follow
    the ArchLinux VCS package guidelines

--- support/pkg/git-version.sh
+++ support/pkg/git-version.sh
@@ -15,4 +15,4 @@
 # You should have received a copy of the GNU General Public License
 # along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
 
-printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)"
+git describe --long | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g'
