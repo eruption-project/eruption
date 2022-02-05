8afcdbe9a98ed919cfa2a9b672189651a7af5cd8
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2022-02-05 20:38:12 +0100

    Add copyright notice
    Update all files, add a copyright notice

--- eruption-process-monitor/src/sensors/process.rs
+++ eruption-process-monitor/src/sensors/process.rs
@@ -13,6 +13,8 @@
 
     You should have received a copy of the GNU General Public License
     along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (c) 2019-2022, The Eruption Development Team
 */
 
 use super::Sensor;
--- sdk/lib/python/eruption/util.py
+++ sdk/lib/python/eruption/util.py
@@ -12,3 +12,5 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Copyright (c) 2019-2022, The Eruption Development Team
