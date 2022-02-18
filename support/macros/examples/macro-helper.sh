d7a5b26ef913db680f88a8229a1dacb6af9010d1
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2022-02-18 09:11:54 +0100

    Provide `macro-helper.sh` shell script
    Ship an example shell script, to be used as a reference for writing Lua
    macros that call out to binaries interacting with the user's session

---
+++ support/macros/examples/macro-helper.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This file is part of Eruption.
+
+# Eruption is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Eruption is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
+
+# Copyright (c) 2019-2022, The Eruption Development Team
+
+
+# Please use this file as a reference for running shell scripts
+# from Lua macros, as your own Linux user instead of root
+#
+# Invoke it from a Lua script like this:
+#
+# result = system("/usr/bin/sudo", {"-u", "user", "/home/user/.local/bin/macro-helper.sh"})
+# if result ~= 0 then
+#     error("Command execution failed with result: " .. result)
+# end
+
+USER=`whoami`
+USER_ID=`id -u`
+DISPLAY=":1"
+
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/$USER/$USER_ID/bus
+
+export DISPLAY=$DISPLAY
+export DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS
+
+/usr/bin/notify-send "Message from Eruption"
