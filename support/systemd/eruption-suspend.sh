a0d3d8781617223f828e56da7de9af4ede0ad7b8
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2022-02-25 17:53:32 +0100

    Add copyright notice

--- support/systemd/eruption-suspend.sh
+++ support/systemd/eruption-suspend.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
+#    This file is part of Eruption.
+#
+#    Eruption is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Eruption is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Copyright (c) 2019-2022, The Eruption Development Team
+
+
 if [ "$1" = "pre" ] ; then
     # prepare Eruption for system sleep
+
     touch /run/lock/eruption-hotplug-helper.lock
 
     systemctl stop eruption-hotplug-helper.service
@@ -10,7 +29,10 @@ if [ "$1" = "pre" ] ; then
     touch /run/lock/eruption-sleep.lock
 else
     # wake up Eruption after system sleep
+
     rm /run/lock/eruption-hotplug-helper.lock
+
     systemctl start eruption-hotplug-helper.service
+
     rm /run/lock/eruption-sleep.lock
 fi
