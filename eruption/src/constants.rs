cddc5cf4155817a5301eb971c01c9bdb79a07cda
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2022-10-01 02:12:31 +0200

    Tighten timeouts

--- eruption/src/constants.rs
+++ eruption/src/constants.rs
@@ -92,7 +92,7 @@ pub const CANVAS_HEIGHT: usize = 6;
 pub const NET_BUFFER_CAPACITY: usize = 4096;
 
 /// Timeout for waiting on condition variables of Lua upcalls
-pub const TIMEOUT_CONDITION_MILLIS: u64 = 100;
+pub const TIMEOUT_CONDITION_MILLIS: u64 = 25;
 
 /// Max number of events that will be processed in each iteration of the main loop
 pub const MAX_EVENTS_PER_ITERATION: u64 = 128;
@@ -101,7 +101,7 @@ pub const MAX_EVENTS_PER_ITERATION: u64 = 128;
 pub const EVENTS_UPCALL_RATE_LIMIT_MILLIS: u64 = 25;
 
 /// Amount of time that has to pass before we retry sending a command to the LED control device
-pub const DEVICE_SETTLE_MILLIS: u64 = 25;
+pub const DEVICE_SETTLE_MILLIS: u64 = 15;
 
 /// Update sensors every n seconds
 /// It is recommended to use a prime number value here
