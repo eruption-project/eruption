c8a7bc13a478fdd62c044c84bc75a9b4eaa0f7e3
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2022-10-09 12:09:20 +0200

    Format source code

--- support/systemd/eruption-suspend.sh
+++ support/systemd/eruption-suspend.sh
@@ -24,7 +24,7 @@ if [ "$1" = "pre" ] ; then
     touch /run/lock/eruption-hotplug-helper.lock
 
     systemctl stop eruption-hotplug-helper.service
-	  systemctl stop eruption.service
+	systemctl stop eruption.service
 
     touch /run/lock/eruption-sleep.lock
 else
