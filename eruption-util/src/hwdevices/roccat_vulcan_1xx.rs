b530229734de2915699293bbc722eaab78aaf26a
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2022-10-12 11:21:26 +0200

    Fix `u8` overflow crash bug
    If the `EV_TO_INDEX_ISO` table contains a sentinel of `0xFF` we can't just
    add 1 to it without overflowing

--- eruption-util/src/hwdevices/roccat_vulcan_1xx.rs
+++ eruption-util/src/hwdevices/roccat_vulcan_1xx.rs
@@ -535,7 +535,7 @@ impl DeviceTrait for RoccatVulcan1xx {
     }
 
     fn ev_key_to_key_index(&self, key: EV_KEY) -> u8 {
-        EV_TO_INDEX_ISO[((key as u8) as usize)] + 1
+        EV_TO_INDEX_ISO[((key as u8) as usize)].saturating_add(1)
     }
 
     fn hid_event_code_to_key_index(&self, code: &KeyboardHidEventCode) -> u8 {
