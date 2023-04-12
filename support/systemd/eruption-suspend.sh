604e49680dc770826aca15f91737138960f37738
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2023-04-12 11:04:22 +0200

    Fix systemd units
    Disable special handling for Eruption on system suspend.
    (This is highly experimental)

--- support/systemd/eruption-suspend.sh
+++ support/systemd/eruption-suspend.sh
@@ -18,24 +18,24 @@
 #
 #  Copyright (c) 2019-2023, The Eruption Development Team
 
-if [ "$1" = "pre" ] ; then
-    # prepare Eruption for system sleep
-
-    touch /run/lock/eruption-hotplug-helper.lock
-
-    systemctl stop eruption-hotplug-helper.service
-    systemctl stop eruption.service
-
-    touch /run/lock/eruption-sleep.lock
-else
-    # wake up Eruption after system sleep
-
-    systemctl reset-failed eruption-hotplug-helper.service
-    systemctl reset-failed eruption.service
-
-    rm /run/lock/eruption-hotplug-helper.lock
-
-    systemctl start eruption-hotplug-helper.service
-
-    rm /run/lock/eruption-sleep.lock
-fi
+# if [ "$1" = "pre" ] ; then
+#     # prepare Eruption for system sleep
+# 
+#     touch /run/lock/eruption-hotplug-helper.lock
+# 
+#     systemctl stop eruption-hotplug-helper.service
+#     systemctl stop eruption.service
+# 
+#     touch /run/lock/eruption-sleep.lock
+# else
+#     # wake up Eruption after system sleep
+# 
+#     systemctl reset-failed eruption-hotplug-helper.service
+#     systemctl reset-failed eruption.service
+# 
+#     rm /run/lock/eruption-hotplug-helper.lock
+# 
+#     systemctl start eruption-hotplug-helper.service
+# 
+#     rm /run/lock/eruption-sleep.lock
+# fi
