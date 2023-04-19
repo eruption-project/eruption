ed06ee15a00cbedde35cec65b6ff727cd9506829
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2023-04-19 05:37:57 +0200

    Add new status subcommand
    Provide a way to query daemon stats

--- eruptionctl/src/subcommands/status.rs
+++ eruptionctl/src/subcommands/status.rs
@@ -19,32 +19,51 @@
     Copyright (c) 2019-2023, The Eruption Development Team
 */
 
-use color_eyre::Help;
+use color_eyre::{owo_colors::OwoColorize, Help};
 use colored::*;
-use dbus::nonblock::stdintf::org_freedesktop_dbus::Properties;
+use dbus::{blocking::Connection, nonblock::stdintf::org_freedesktop_dbus::Properties};
 use eyre::Context;
+use std::{fmt::Display, time::Duration};
 
-use crate::dbus_client::dbus_system_bus;
+use crate::{
+    constants,
+    dbus_client::{
+        config::OrgEruptionConfig, dbus_system_bus, profile::OrgEruptionProfile,
+        slot::OrgEruptionSlot,
+    },
+};
 
 type Result<T> = std::result::Result<T, eyre::Error>;
 
 /// Sub-commands of the "status" command
 #[derive(Debug, clap::Parser)]
 pub enum StatusSubcommands {
-    /// Shows the currently active profile
+    /// Shows some status informations about Eruption
     #[clap(display_order = 0)]
+    Daemon,
+
+    /// Shows the currently active profile
+    #[clap(display_order = 1)]
     Profile,
 
     /// Shows the currently active slot
-    #[clap(display_order = 1)]
+    #[clap(display_order = 2)]
     Slot,
 }
 
 pub async fn handle_command(command: StatusSubcommands) -> Result<()> {
     match command {
+        StatusSubcommands::Daemon => daemon_command().await,
         StatusSubcommands::Profile => profile_command().await,
         StatusSubcommands::Slot => slot_command().await,
     }
+}
+
+async fn daemon_command() -> Result<()> {
+    let daemon_status = get_daemon_status().await?;
+    println!("{}\n\n{daemon_status}", "Eruption status".bold());
+
+    Ok(())
 }
 
 async fn profile_command() -> Result<()> {
@@ -66,6 +85,114 @@ async fn slot_command() -> Result<()> {
     println!("Current slot: {}", format!("{index}").bold());
 
     Ok(())
+}
+
+/// Status information about the running Eruption daemons
+struct DaemonStatus {
+    /// Status of the Eruption daemon
+    pub status: String,
+
+    /// List of slot names
+    pub slot_names: Vec<String>,
+
+    /// List of associated profiles for each slot
+    pub slot_profiles: Vec<String>,
+
+    /// List of available profiles
+    pub profiles: Vec<(String, String)>,
+
+    /// Currently active profile
+    pub active_profile: String,
+
+    /// Currently active slot
+    pub active_slot: usize,
+}
+
+impl Display for DaemonStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Eruption daemon: {}\nLoaded Profiles: {}\n",
+            self.status.bold(),
+            self.profiles.len()
+        )?;
+
+        write!(
+            f,
+            "Active Profile: {}\nActive Slot: {}\n\nSlots:\n",
+            self.active_profile.bold(),
+            (self.active_slot + 1).bold()
+        )?;
+
+        for (index, (slot_name, profile)) in self
+            .slot_names
+            .iter()
+            .zip(self.slot_profiles.iter())
+            .enumerate()
+        {
+            writeln!(
+                f,
+                "{}: {}: {}",
+                (index + 1).dimmed(),
+                slot_name.bold(),
+                profile
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
+/// Returns a few stats about the running Eruption daemons
+async fn get_daemon_status() -> Result<DaemonStatus> {
+    let conn = Connection::new_system()?;
+
+    let config_proxy = conn.with_proxy(
+        "org.eruption",
+        "/org/eruption/config",
+        Duration::from_secs(constants::DBUS_TIMEOUT_MILLIS as u64),
+    );
+
+    let profiles_proxy = conn.with_proxy(
+        "org.eruption",
+        "/org/eruption/profile",
+        Duration::from_secs(constants::DBUS_TIMEOUT_MILLIS as u64),
+    );
+
+    let slot_proxy = conn.with_proxy(
+        "org.eruption",
+        "/org/eruption/slot",
+        Duration::from_secs(constants::DBUS_TIMEOUT_MILLIS as u64),
+    );
+
+    // let status_proxy = conn.with_proxy(
+    //     "org.eruption",
+    //     "/org/eruption/status",
+    //     Duration::from_secs(constants::DBUS_TIMEOUT_MILLIS as u64),
+    // );
+
+    let status = if config_proxy.ping()? {
+        "OK"
+    } else {
+        "<no response>"
+    }
+    .to_string();
+
+    let slot_names = slot_proxy.slot_names()?;
+    let slot_profiles = slot_proxy.get_slot_profiles()?;
+    let profiles = profiles_proxy.enum_profiles()?;
+
+    let active_profile = get_active_profile().await?;
+    let active_slot = get_active_slot().await?;
+
+    Ok(DaemonStatus {
+        status,
+        slot_names,
+        slot_profiles,
+        profiles,
+        active_profile,
+        active_slot,
+    })
 }
 
 /// Get the name of the currently active profile
