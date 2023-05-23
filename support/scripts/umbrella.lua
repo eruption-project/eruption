1a2f97ee2c3a5a70228f4552938b8d6bced9c497
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2023-05-23 05:09:57 +0200

    Add first effect utilizing the new accelerated 2D raster API

---
+++ support/scripts/umbrella.lua
@@ -0,0 +1,44 @@
+-- SPDX-License-Identifier: GPL-3.0-or-later
+--
+-- This file is part of Eruption.
+--
+-- Eruption is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Eruption is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Copyright (c) 2019-2023, The Eruption Development Team
+--
+require "declarations"
+require "debug"
+
+-- global state variables --
+ticks = 0
+offsets = {0, 0, 0}
+
+-- event handler functions --
+function on_mouse_move(rel_x, rel_y, rel_z)
+    offsets[1] = offsets[1] - rel_x
+    offsets[2] = offsets[2] - rel_y
+    offsets[3] = offsets[3] - rel_z
+end
+
+function on_render()
+    local canvas = get_canvas()
+
+    -- testing simplex noise instead of the umbrella effect for now
+    draw_simplex_noise(canvas, 0, 0, canvas_width, canvas_height,
+                       offsets[1] / 40, offsets[2] / 40, ticks, 0.25)
+
+    realize_canvas(canvas)
+end
+
+function on_tick(delta) ticks = ticks + delta end
