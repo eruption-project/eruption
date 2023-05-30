6606b24a7c551717017d0e1d48fefbb8570d02e6
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2023-05-30 06:11:45 +0200

    Add benchmarking infrastructure based on criterion

---
+++ eruption/benches/benchmark.rs
@@ -0,0 +1,37 @@
+/*  SPDX-License-Identifier: GPL-3.0-or-later  */
+
+/*
+    This file is part of Eruption.
+
+    Eruption is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Eruption is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Eruption.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright (c) 2019-2023, The Eruption Development Team
+*/
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+// fn fibonacci(n: u64) -> u64 {
+//     match n {
+//         0 => 1,
+//         1 => 1,
+//         n => fibonacci(n - 1) + fibonacci(n - 2),
+//     }
+// }
+
+fn criterion_benchmark(c: &mut Criterion) {
+    // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
