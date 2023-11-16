a122284a849faf7ff607b2f9a7443dd0960c38cc
X3n0m0rph59 <x3n0m0rph59@gmail.com>
2023-11-16 03:37:01 +0100

    Update constants
    Lower the resolution of the canvas somewhat.
    Code cleanups

--- eruption/src/constants.rs
+++ eruption/src/constants.rs
@@ -162,15 +162,6 @@ pub const DEVICE_SETTLE_DELAY: u64 = 250;
 /// It is recommended to use a prime number value here
 pub const SENSOR_UPDATE_TICKS: u64 = TARGET_FPS_LIMIT / 2;
 
-/// Timeout used for trying to acquire a thread synchronization primitive
-pub const LOCK_CONTENDED_WAIT_MILLIS_SHORT: Duration = Duration::from_millis(11);
-
-/// Timeout used for trying to acquire a thread synchronization primitive
-pub const LOCK_CONTENDED_WAIT_MILLIS: Duration = Duration::from_millis(19);
-
-/// Timeout used for trying to acquire a thread synchronization primitive
-pub const LOCK_CONTENDED_WAIT_MILLIS_LONG: Duration = Duration::from_millis(37);
-
 /// Generic timeout value for fast operations
 pub const TIMEOUT_MILLIS_SHORT: u64 = 500;
 
@@ -203,7 +194,7 @@ pub const MAX_KEYS: usize = 256;
 /// Max. supported number of mouse buttons
 pub const MAX_MOUSE_BUTTONS: usize = 32;
 
-/// Limit event handler upcalls to 1 per `EVENTS_UPCALL_RATE_LIMIT_MILLIS` milliseconds
+/// Limit event handler upcalls to 1 every `EVENTS_UPCALL_RATE_LIMIT_MILLIS` milliseconds
 pub const EVENTS_UPCALL_RATE_LIMIT_MILLIS: u64 = 16;
 
 /// Limit framerate to n frames per second
@@ -214,23 +205,23 @@ pub const TIMER_TPS: u64 = 60;
 
 /// The width of the canvas (max. reasonable value approx. 128)
 /// NOTE: Values considerably larger than 128 currently lead to stuttering in the Eruption GUI
-pub const CANVAS_WIDTH: usize = 86;
+pub const CANVAS_WIDTH: usize = 64; // 86;
 
 /// The height of the canvas (max. reasonable value approx. 128)
 /// NOTE: Values considerably larger than 128 currently lead to stuttering in the Eruption GUI
-pub const CANVAS_HEIGHT: usize = 64;
+pub const CANVAS_HEIGHT: usize = 48; // 64;
 
 /// The number of "pixels" on the canvas
 pub const CANVAS_SIZE: usize = CANVAS_WIDTH * CANVAS_HEIGHT;
 
 /// Timeout for waiting on condition variables of Lua upcalls
-pub const TIMEOUT_CONDITION_MILLIS: u64 = 40;
+pub const TIMEOUT_UPCALL_MILLIS: u64 = 250;
 
 /// Timeout for waiting on ready condition after the RealizeColorMap upcall
-pub const TIMEOUT_REALIZE_COLOR_MAP_CONDITION_MILLIS: u64 = 40;
+pub const TIMEOUT_REALIZE_COLOR_MAP_MILLIS: u64 = 40;
 
 /// Max number of events that will be processed in each iteration of the main loop
-pub const MAX_EVENTS_PER_ITERATION: u64 = 256;
+pub const MAX_EVENTS_PER_ITERATION: u64 = 1024;
 
 /// Fade in on profile switch for n milliseconds
 pub const FADE_MILLIS: u64 = 1333;
